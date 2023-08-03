4a157102e2700693d849fa67456c78f5dcba248a
norlock <jwillems25@hotmail.com>
2023-08-03 15:38:36 +0200

    extra gui options

--- src/model/gui.rs
+++ src/model/gui.rs
@@ -1,6 +1,6 @@
 use super::AppState;
 use crate::InitialiseApp;
-use egui::{Color32, Context, RichText};
+use egui::{Color32, Context, RichText, Ui};
 use glam::Vec3;
 
 pub struct GuiState {
@@ -49,27 +49,33 @@ impl GuiState {
 
         self.update_labels(app_state);
 
-        let fps_text = RichText::new(&self.fps_text).color(Color32::WHITE);
-        let cpu_time_text = RichText::new(&self.cpu_time_text).color(Color32::WHITE);
-        let elapsed_text = RichText::new(&self.elapsed_text).color(Color32::WHITE);
+        let emitter = &mut app_state.compute.emitter;
 
         egui::Window::new("Emitter settings").show(ctx, |ui| {
-            ui.label(fps_text);
-            ui.label(cpu_time_text);
-            ui.label(elapsed_text);
+            create_label(ui, &self.fps_text);
+            create_label(ui, &self.cpu_time_text);
+            create_label(ui, &self.elapsed_text);
 
             if ui.button("Reset camera").clicked() {
                 app_state.camera.reset();
             }
+            ui.add_space(5.0);
 
             ui.add(egui::Slider::new(&mut self.box_yaw, 0.0..=360.0).text("Box yaw"));
             ui.add(egui::Slider::new(&mut self.box_pitch, 0.0..=360.0).text("Box pitch"));
             ui.add(egui::Slider::new(&mut self.box_roll, 0.0..=360.0).text("Box roll"));
             ui.add(egui::Slider::new(&mut self.diff_width, 0.0..=360.0).text("Diffusion width"));
             ui.add(egui::Slider::new(&mut self.diff_depth, 0.0..=360.0).text("Diffusion depth"));
-        });
 
-        let emitter = &mut app_state.compute.emitter;
+            ui.add_space(5.0);
+            create_label(ui, "Box dimensions (w, h, d)");
+
+            ui.horizontal(|ui| {
+                create_drag_value(ui, &mut emitter.box_dimensions.x);
+                create_drag_value(ui, &mut emitter.box_dimensions.y);
+                create_drag_value(ui, &mut emitter.box_dimensions.z);
+            });
+        });
 
         emitter.box_rotation = Vec3::new(
             self.box_yaw.to_radians(),
@@ -80,4 +86,17 @@ impl GuiState {
         emitter.diffusion_width_rad = self.diff_width.to_radians();
         emitter.diffusion_depth_rad = self.diff_depth.to_radians();
     }
+}
+
+fn create_label(ui: &mut Ui, str: &str) {
+    ui.label(RichText::new(str).color(Color32::WHITE));
+    ui.add_space(5.0);
+}
+
+fn create_drag_value(ui: &mut Ui, val: &mut f32) {
+    ui.add(
+        egui::DragValue::new(val)
+            .clamp_range(0f64..=f64::MAX)
+            .speed(0.1),
+    );
 }
